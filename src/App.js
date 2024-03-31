f61ad13f2782b51d9c7c5480cf8f092010a5026d
Jabbar9 <saj8180025653@gmail.com>
2024-03-31 17:18:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+const searchIcon = require("./assets/search.png")
+const castIcon = require("./assets/cast.png")
+const profileIcon = require("./assets/profile.gif")
+const Temp = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div className="main">
+        <div className="navbar">
+          <p className="title">Music</p>
+          <div className="navbars-section">
+            <ul>
+              <li>Home</li>
+              <li>Explore</li>
+              <li>Library</li>
+              <li>Booking</li>
+            </ul>
+          </div>
+          <div className="searchBar">            
+            <img alt="play" className="searchIcon" src={searchIcon} />
+            <input className="inputBar" type="text" placeholder="Search" />
+          </div>
+          <div className="icons">
+            <img alt="cast" className="cast" src={castIcon}/>            
+            <img alt="cast" className="cast" src={profileIcon}/>            
+          </div>
+        </div>
 
-export default App;
+        <div className="artist-section">
+        <div className="intro-section">
+          <img alt="profile" className="proPhoto" src={profileIcon}/>
+          <h1>Welcome back Abdul</h1>
+          <h3>LETS TRAVEL AROUND THE WORLD</h3>
+        </div>
+      </div>
+      </div>      
+    </div>    
+  );
+};
+
+export default Temp;
